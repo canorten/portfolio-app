84ae567cc7ff28b626dcf3168a00eded3a989465
canorten <60063196+canorten@users.noreply.github.com>
2025-11-30 02:16:05 +0300

    Configure Next.js for static export and GitHub Pages
    Set output to 'export' for static HTML generation and configure images for GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // 1. Statik HTML çıktısı almasını sağlar
+  images: {
+    unoptimized: true, // 2. GitHub Pages'de resim optimizasyon sunucusu olmadığı için bunu kapatmalıyız
+  },
+  basePath: '/portfolio-app',
 };
 
 export default nextConfig;
